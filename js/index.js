7f590c99f45b626b9f092f7f310e741a0a365171
wendy198622 <wendy198622@gmail.com>
2020-08-18 16:05:54 +0800

    完成檔案更新

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+
+$(document).ready(function(){
+
+
+  new WOW().init();
+
+  $('.slider').slick({
+      infinite: true,
+      autoplay: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: true
+  });
+
+    
+});
+
+
